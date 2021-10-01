6cd94163473c73d4f45aaf03e20dafe9d1519531
GlebZyryanov <darrenshen@gmail.com>
2021-10-01 22:31:31 +0700

    "Сделал структуру точки и добавил функции в хед файл и описал их в сппшнике"

--- touchkah.h
+++ touchkah.h
@@ -1,1 +1,14 @@
 #pragma once
+#ifndef touchkah_h
+#define touchkah_h
+
+struct Touchka {
+	int Cord1;//координаты точки
+	int Cord2;
+};
+void initTouch(struct Touchka* T);
+void setTouch(struct Touchka* T);
+void printTouch(struct Touchka* T);
+
+#endif //Header
+
